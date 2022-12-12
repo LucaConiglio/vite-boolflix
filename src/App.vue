77d799ead8f2a644c893e064c3ea76884404be36
LucaConiglio <lucaconiglio.92@gmail.com>
2022-12-12 17:19:06 +0100

    inizio chiamata axios

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import TheMain from './components/TheMain.vue';
+import {store, fetchMovies } from './store';
+import TheHeader from './components/TheHeader.vue';
+export default {
+  components : {TheMain, TheHeader },
+  data() {
+    return {
+      store
+    }
+  }
+
+}
+
+
+</script>
+
+<template>
+  <TheHeader @search="onClickSearch"></TheHeader>
+  <TheMain></TheMain>
+</template>
+
+<style scoped>
+
+</style>
