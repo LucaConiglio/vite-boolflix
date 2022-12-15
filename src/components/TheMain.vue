c1182755d9ae4cd282e9f77ea6b63a80b7b45dd1
LucaConiglio <lucaconiglio.92@gmail.com>
2022-12-15 18:02:04 +0100

    migliorata la card

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -3,16 +3,16 @@
     <div class="container pb-5">
       <h2 class=" py-4 text-white" v-if="store.show">films</h2>
       <div class="row g-3 gy-5">
-        <div v-for="(film, index) in store.movies" :key="index" class="col-xl-2 col-lg-3 col-sm-4  d-flex flex-column">
-        <Card :film="film"></Card>
+        <div v-for="(singlePoster, index) in store.movies" :key="index" class="col-xl-2 col-lg-3 col-sm-4  d-flex flex-column">
+        <Card :poster="singlePoster"></Card>
          
         </div>
       </div>
 
       <h2 class="py-4 text-white" v-if="store.show">Tv-Series</h2>
       <div class="row g-3 gy-5">
-       <div v-for="(series, index) in store.tvSeries" :key="index" class="col-xl-2 col-lg-3 col-sm-4 d-flex flex-column ">
-         <Card2 :series="series"></Card2>
+       <div v-for="(singlePoster, index) in store.tvSeries" :key="index" class="col-xl-2 col-lg-3 col-sm-4 d-flex flex-column ">
+        <Card :poster="singlePoster"></Card>
        </div>
       </div>
     </div>
@@ -21,9 +21,8 @@
 <script>
 import {store} from '../store'
 import Card from '../components/Card.vue'
-import Card2 from '../components/Card2.vue'
 export default {
-  components : { Card, Card2,},
+  components : { Card, },
   data() {
     return {
       store,
